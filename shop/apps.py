0ccb2bbb9d8963e44ba4d53d4a6aa903770ea245
PROGRAMMER : MR.  AMIT <167705391+Techhackontime999@users.noreply.github.com>
2025-04-28 19:45:56 +0530

    Update apps.py

--- shop/apps.py
+++ shop/apps.py
@@ -1,17 +1,7 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import Group
 
 
 class ShopConfig(AppConfig):
     name = 'shop'
 
 
-    def ready(self):
-        # Create 'customers' group if it doesn't exist
-        from django.db.models import signals
-        from django.contrib.auth.models import Group
-
-        # Ensure the group is created when the app is ready
-        group, created = Group.objects.get_or_create(name='customers')
-        group, created = Group.objects.get_or_create(name='sellers')
-        group, created = Group.objects.get_or_create(name='webadmin')
