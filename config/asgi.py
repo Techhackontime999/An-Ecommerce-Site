084f92c160e50985217f7d3d9e4920b64862887a
MR. AMIT KUMAR <167705391+Techhackontime999@users.noreply.github.com>
2026-07-29 07:10:12 +0530

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,9 +1,21 @@
-"""ASGI config for config project."""
+# """ASGI config for config project."""
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_asgi_application()
+
+
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings.production"
+)
 
 application = get_asgi_application()
