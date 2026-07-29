8034959edb7bee7980c043e71316f574e9fa6597
MR. AMIT KUMAR <167705391+Techhackontime999@users.noreply.github.com>
2026-07-29 07:24:27 +0530

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,21 +1,10 @@
-# """WSGI config for config project."""
-
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# application = get_wsgi_application()
-
 """WSGI config for config project."""
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "config.settings.production"
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
