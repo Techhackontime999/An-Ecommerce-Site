78f2c8dc44b7e2872665ef0773f1f609c4c38281
Techhackontime999 <amitkumarkh01012006@gmail.com>
2026-07-30 09:07:13 +0530

    fix: correct STATIC_ROOT and STATICFILES_DIRS to use PROJECT_DIR instead of BASE_DIR

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,6 +3,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(BASE_DIR)
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-local-development-only")
 
@@ -97,9 +98,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
