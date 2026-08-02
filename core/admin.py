51a3af9d1ffc1194a00baf03e14a87448548bd4c
Techhackontime999 <amitkumarkh01012006@gmail.com>
2026-08-03 05:27:17 +0530

    Add quick options for all admin models and expand dashboard stats

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 import django
 
+from django.apps import apps
 from django.contrib import admin
 from django.conf import settings
 from django.db.models import Sum
@@ -13,6 +14,28 @@ from django.contrib.auth.models import User
 
 _orig_index = admin.site.index
 
+STAT_MODELS = {
+    'seller_product_count': 'seller.SellerProduct',
+    'blog_post_count': 'blogs.Post',
+    'coupon_count': 'coupons.Coupon',
+    'deal_count': 'deals.Deal',
+    'subscriber_count': 'newsletter.Subscriber',
+    'message_count': 'contact.ContactMessage',
+    'faq_count': 'faq.FAQ',
+    'story_count': 'faq.Story',
+    'news_count': 'news.NewsItem',
+    'service_count': 'services.Service',
+    'documentation_count': 'documentation.DocumentationSection',
+    'about_count': 'about.AboutSection',
+    'team_count': 'about.TeamMember',
+    'shipment_count': 'shipping.Shipment',
+    'notification_count': 'notifications.Notification',
+}
+
+
+def _model_count(label):
+    return apps.get_model(label).objects.count()
+
 
 def patched_index(request, extra_context=None):
     extra_context = extra_context or {}
@@ -25,6 +48,8 @@ def patched_index(request, extra_context=None):
     extra_context['debug'] = settings.DEBUG
     total = Payment.objects.filter(status='captured').aggregate(Sum('amount'))
     extra_context['total_revenue'] = total['amount__sum'] or 0
+    for key, label in STAT_MODELS.items():
+        extra_context[key] = _model_count(label)
     return _orig_index(request, extra_context=extra_context)
 
 
